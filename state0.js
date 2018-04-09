23da83c0c09aa333d9442ffe5567e2233bb31d94
zlatkotrajcevski <38009352+zlatkotrajcevski@users.noreply.github.com>
2018-04-09 21:42:20 +0200

    Add files via upload

--- state0.js
+++ state0.js
@@ -5,14 +5,30 @@ demo.state0.prototype = {
     create: function(){
         game.state.backgroundColor = '#80ff80';
         console.log('state0');
-        game.input.keybord.addKey(Phaser.Keybord.ONE).onDown.add(changeState, null, null, 1);
-        
-        game.input.keybord.addKey(Phaser.Keybord.ZERO).onDown.add(changeState, null, null, 0);
+        addChangeStateEventListeners();
+        game.scale.scaleMode = Phaser.scaleMenager.SHOW_ALL;
+   
     },
     update: function(){}    
 };
 
 function changeState(i, stateNum){
-    console.log(i);
     game.state.start('state' + stateNum);
+}
+function addKeyCallback(key, fn, args){
+    game.input.keybord.addKey(key).omDown.add(fn, null, null, args);
+}
+
+function addChangeStateEventListeners(){
+    addKeyCallback(Phaser.Keybord.ZERO, changeState, 0);
+    addKeyCallback(Phaser.Keybord.ONE, changeState, 1);
+    addKeyCallback(Phaser.Keybord.TWO, changeState, 2);
+    addKeyCallback(Phaser.Keybord.THREE, changeState, 3);
+    addKeyCallback(Phaser.Keybord.FOUR, changeState, 4);
+    addKeyCallback(Phaser.Keybord.FIVE, changeState, 5);
+    addKeyCallback(Phaser.Keybord.SIX, changeState, 6);
+    addKeyCallback(Phaser.Keybord.SEVEN, changeState, 7);
+    addKeyCallback(Phaser.Keybord.EIGHT, changeState, 8);
+    addKeyCallback(Phaser.Keybord.NINE, changeState, 9);
+    
 }
