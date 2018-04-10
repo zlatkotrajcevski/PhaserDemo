9da7ea129d7fad3ff6ca2bff878432712cb128fa
zlatkotrajcevski <38009352+zlatkotrajcevski@users.noreply.github.com>
2018-04-10 17:21:24 +0200

    Add files via upload

--- state6.js
+++ state6.js
@@ -3,7 +3,5 @@ demo.state6.prototype = {
     preload: function(){},
     create: function(){
         demo.state.backgroundColor = '#80ff80';
-        console.log('state6');
-    },
     update: function(){}    
 };
