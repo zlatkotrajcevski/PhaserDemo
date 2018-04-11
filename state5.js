3695b87b385809023d82d06c0d4880b9084dcde7
zlatkotrajcevski <38009352+zlatkotrajcevski@users.noreply.github.com>
2018-04-11 13:21:52 +0200

    Add files via upload

--- state5.js
+++ state5.js
@@ -1,6 +1,43 @@
+var accel = 400, platform, platformGroup;
+
 demo.state5 = function(){};
 demo.state5.prototype = {
-    preload: function(){},
-    create: function(){},
-    update: function(){}    
+  preload: function(){
+    game.load.image('platform', 'assets/sprites/platform.png');
+  },
+  create: function(){
+    game.stage.backgroundColor = '#ff99dd';
+    addChangeStateEventListeners();
+
+    adam = game.add.sprite(centerX, 500, 'adam');
+      adam.scale.setTo(0.555, 0.5);
+    platform = game.add.sprite(0, 800, 'platform');
+    platformGroup = game.add.group();
+    platformGroup.create(650, 400, 'platform');
+    platformGroup.create(1300, 400, 'platform');
+
+    game.physics.enable([adam, platform, platformGroup]);
+
+    adam.body.gravity.y = 500;
+    adam.body.bounce.y = 0.3;
+    adam.body.drag.x = 400;
+    adam.body.collideWorldBounds = true;
+
+    platform.body.immovable = true;
+
+    platformGroup.setAll('body.immovable', true);
+  },
+  update: function(){
+    game.physics.arcade.collide(adam, [platform, platformGroup]);
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+      adam.body.acceleration.x = -accel;
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      adam.body.acceleration.x = accel;
+    } else {
+      adam.body.acceleration.x = 0;
+    }
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+      adam.body.velocity.y = -300;
+    }
+  }
 };
